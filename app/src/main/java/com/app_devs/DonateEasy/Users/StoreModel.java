6bca008c07877d01fbc35968ad89513123300af4
sukhdeepsumit <sukhi.aditya.sumit@gmail.com>
2021-03-17 10:51:48 +0530

    copy paste part1

---
+++ app/src/main/java/com/app_devs/DonateEasy/Users/StoreModel.java
@@ -0,0 +1,13 @@
+package com.app_devs.DonateEasy.Users;
+
+public class StoreModel {
+    public String name, address, distance, duration;
+
+    public StoreModel(String name, String address, String distance, String duration) {
+
+        this.name = name;
+        this.address = address;
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
