ea38a6d501add7784325593281c98a9dc28c5df5
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2021-03-18 13:32:57 +0530

    checking api

--- app/src/main/java/com/app_devs/DonateEasy/Users/DisplayNGO.java
+++ app/src/main/java/com/app_devs/DonateEasy/Users/DisplayNGO.java
@@ -1,60 +1,163 @@
 package com.app_devs.DonateEasy.Users;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.app_devs.DonateEasy.MyProfile;
 import com.app_devs.DonateEasy.R;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisplayNGO extends AppCompatActivity {
 
-    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();;
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+    public static final String TAG = "CurrentLocNearByPlaces";
+    private static final int LOC_REQ_CODE = 1;
+
+    protected GeoDataClient geoDataClient;
+    protected PlaceDetectionClient placeDetectionClient;
+    protected RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_n_g_o);
+
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        tb.setSubtitle("Near by places");
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recyclerView.getContext(),
+                        recyclerLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        getCurrentPlaceItems();
     }
 
+    private void getCurrentPlaceItems() {
+        if (isLocationAccessPermitted()) {
+            getCurrentPlaceData();
+        }
+        else {
+            requestLocationAccessPermission();
+        }
+    }
 
-//    private void checkUserExists() {
-//        firebaseFirestore.collection("Users")
-//                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                .get()
-//                .addOnCompleteListener(task -> {
-//                    if (task.isSuccessful()) {
-//                        if (!task.getResult().exists()) {
-//                            messageAlert();
-//                        }
-//                    }
-//                });
-//    }
-//
-//    private void messageAlert() {
-//        new AlertDialog.Builder(this)
-//                .setTitle("Location Error")
-//                .setMessage("We cannot get your location\nDo you want to set it manually ?")
-//                .setPositiveButton("YES", (dialogInterface, i) -> {
-//                    startActivity(new Intent(getApplicationContext(), MyProfile.class));
-//                    finish();
-//                })
-//                .setNegativeButton("NO", (dialogInterface, i) -> {
-//                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
-//                    finish();
-//                })
-//                .show();
-//    }
-//
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        checkUserExists();
-//    }
+    @SuppressLint("MissingPermission")
+    private void getCurrentPlaceData() {
+        Task<PlaceLikelihoodBufferResponse> placeResult = placeDetectionClient.getCurrentPlace(null);
+        placeResult.addOnCompleteListener(task -> {
+            Log.d(TAG, "current location places info");
+            List<Place> placesList = new ArrayList<>();
+            PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+            for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                placesList.add(placeLikelihood.getPlace().freeze());
+            }
+            likelyPlaces.release();
+
+            PlacesRecyclerViewAdapter recyclerViewAdapter = new
+                    PlacesRecyclerViewAdapter(placesList, getApplicationContext());
+            recyclerView.setAdapter(recyclerViewAdapter);
+        });
+    }
+
+    private boolean isLocationAccessPermitted() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private void requestLocationAccessPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOC_REQ_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOC_REQ_CODE) {
+            if (resultCode == RESULT_OK) {
+                getCurrentPlaceData();
+            }
+        }
+    }
+
+    /*private void checkUserExists() {
+        firebaseFirestore.collection("Users")
+                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        if (!task.getResult().exists()) {
+                            messageAlert();
+                        }
+                    }
+                });
+    }
+
+    private void messageAlert() {
+        new AlertDialog.Builder(this)
+                .setTitle("Location Error")
+                .setMessage("We cannot get your location\nDo you want to set it manually ?")
+                .setPositiveButton("YES", (dialogInterface, i) -> {
+                    startActivity(new Intent(getApplicationContext(), MyProfile.class));
+                    finish();
+                })
+                .setNegativeButton("NO", (dialogInterface, i) -> {
+                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                    finish();
+                })
+                .show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkUserExists();
+    }*/
 
     @Override
     public void onBackPressed() {
---
+++ app/src/main/java/com/app_devs/DonateEasy/Users/PlacesRecyclerViewAdapter.java
@@ -0,0 +1,105 @@
+package com.app_devs.DonateEasy.Users;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.app_devs.DonateEasy.R;
+import com.google.android.gms.location.places.Place;
+
+import java.util.List;
+
+public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecyclerViewAdapter.MyViewHolder> {
+
+    private List<Place> placesList;
+    private Context context;
+
+    public PlacesRecyclerViewAdapter(List<Place> list, Context ctx) {
+        placesList = list;
+        context = ctx;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.places_item, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final int itemPos = position;
+        final Place place = placesList.get(position);
+        holder.name.setText(place.getName());
+        holder.address.setText(place.getAddress());
+        holder.phone.setText(place.getPhoneNumber());
+        if(place.getWebsiteUri() != null){
+            holder.website.setText(place.getWebsiteUri().toString());
+        }
+
+        if(place.getRating() > -1)
+        {
+            holder.ratingBar.setNumStars((int)place.getRating());
+        }
+        else{
+            holder.ratingBar.setVisibility(View.GONE);
+        }
+
+        holder.viewOnMap.setOnClickListener(view -> showOnMap(place));
+    }
+
+    @Override
+    public int getItemCount() {
+        return placesList.size();
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView name;
+        public TextView address;
+        public TextView phone;
+        public TextView website;
+        public RatingBar ratingBar;
+
+        public Button viewOnMap;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            address = itemView.findViewById(R.id.address);
+            phone = itemView.findViewById(R.id.phone);
+            website = itemView.findViewById(R.id.website);
+            ratingBar = itemView.findViewById(R.id.rating);
+
+            viewOnMap = itemView.findViewById(R.id.view_map_b);
+        }
+    }
+
+    private void showOnMap(Place place){
+        FragmentManager fm = ((Dashboard)context)
+                .getSupportFragmentManager();
+
+        Bundle bundle=new Bundle();
+        bundle.putString("name", (String)place.getName());
+        bundle.putString("address", (String)place.getAddress());
+        bundle.putDouble("lat", place.getLatLng().latitude);
+        bundle.putDouble("lng", place.getLatLng().longitude);
+
+        PlaceOnMapFragment placeFragment = new PlaceOnMapFragment();
+        placeFragment.setArguments(bundle);
+
+        fm.beginTransaction().replace(R.id.map_frame, placeFragment).commit();
+    }
+}
